6b69aadfd2c7213477c2ddfc63888d2728b5764e
imadbenmadi <imadbenmadi891@gmail.com>
2024-07-08 19:43:33 +0100

    Remove unused List_Card component, update imports, and format code in Router.jsx

--- src/Components/Institustions/Item.jsx
+++ src/Components/Institustions/Item.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import { useLocation } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 function InstitutionItem() {
     const location = useLocation();
     const institution_id = location.pathname.split("/")[2];
@@ -69,21 +69,57 @@ function InstitutionItem() {
 
     return (
         <div className="container mx-auto px-4 py-8">
-            <div className="flex justify-between items-center mb-6">
-                <div>
+            <div className="flex flex-wrap sm:justify-between justify-center gap-8  sm:gap-24 mb-12">
+                <div className=" flex flex-col gap-3 text-black_text">
                     <h1 className="text-3xl font-bold">{institution.Name}</h1>
-                    <p className="text-lg text-gray_v">
-                        {institution.Wilaya}/{institution.Location}
-                    </p>
+                    <div className=" pl-6 flex flex-col gap-2">
+                        <p className="text-lg text-gray_v">
+                            Wilaya :{" "}
+                            <span className="text-md font-semibold">
+                                {institution.Wilaya}
+                            </span>
+                        </p>
+                        <p className="text-lg text-gray_v">
+                            Location :{" "}
+                            <span className="text-md font-semibold">
+                                {institution.Location}
+                            </span>
+                        </p>
+                        <p>
+                            <span className="text-lg text-gray_v">Type :</span>{" "}
+                            <span className="text-md font-semibold">
+                                {institution.Type}
+                            </span>
+                        </p>
+                    </div>
                 </div>
-                <button className="bg-blue_v text-white px-4 py-2 rounded">
-                    See events
-                </button>
+                <div className=" flex flex-col gap-6 text-center">
+                    <Link
+                        to={`/Institustions/${institution_id}/Events`}
+                        className="border-2 border-blue_v text-blue_v font-semibold px-4 py-2 rounded-md"
+                    >
+                        See events
+                    </Link>
+                    <Link
+                        to={`/Institustions/${institution_id}/Edit`}
+                        className="border-2 border-green_v text-green_v font-semibold
+                        cursor-pointer
+                        px-4 py-2 rounded-md"
+                    >
+                        Edite Institustion
+                    </Link>
+                    <div
+                        className=" bg-red-500 cursor-pointer
+                     text-white font-semibold px-4 py-2 rounded-md"
+                    >
+                        Delete Institustion
+                    </div>
+                </div>
             </div>
             {director && (
                 <div className="mb-6">
                     <h2 className="text-2xl font-semibold mb-2">Director</h2>
-                    <div className="flex gap-4">
+                    <div className="flex flex-wrap gap-4">
                         <div className="border p-2 rounded-md flex items-center justify-between gap-2 text-sm font-semibold min-w-[300px]">
                             <span>Email: {director.email}</span>
                             <button className="text-blue_v">✏️</button>
@@ -95,38 +131,50 @@ function InstitutionItem() {
                     </div>
                 </div>
             )}
-            <div>
+            <div className=" mt-12">
                 <h2 className="text-2xl font-semibold mb-2">Doctors</h2>
-                <table className="table-auto w-full text-sm">
-                    <thead>
-                        <tr className="bg-gray_v font-normal">
-                            <th className="px-4 py-2">Name</th>
-                            <th className="px-4 py-2">Wilaya</th>
-                            <th className="px-4 py-2">Localisation</th>
-                            <th className="px-4 py-2">Doctor Profile</th>
-                        </tr>
-                    </thead>
-                    <tbody className="text-center font-semibold">
-                        {institution.Medecins.map((doctor, index) => (
-                            <tr key={index}>
-                                <td className="border px-4 py-2">
-                                    {doctor.name}
-                                </td>
-                                <td className="border px-4 py-2">
-                                    {doctor.wilaya}
-                                </td>
-                                <td className="border px-4 py-2">
-                                    {doctor.localisation}
-                                </td>
-                                <td className="border px-4 py-2">
-                                    <button className="bg-blue_v text-white px-4 py-2 rounded">
-                                        See Profile
-                                    </button>
-                                </td>
+                {institution.Medecins.length === 0 ? (
+                    <p className="text-gray_v text-center font-semibold pt-6">
+                        No doctors found
+                    </p>
+                ) : (
+                    <table className="table-auto w-full text-sm">
+                        <thead>
+                            <tr className="bg-gray_white font-normal">
+                                <th className="px-4 border py-2  rounded-tl-lg">
+                                    Name
+                                </th>
+                                <th className="px-4 border py-2">Wilaya</th>
+                                <th className="px-4 border py-2">
+                                    Localisation
+                                </th>
+                                <th className="px-4 border py-2 rounded-tr-lg">
+                                    Doctor Profile
+                                </th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody className="text-center font-semibold">
+                            {institution.Medecins.map((doctor, index) => (
+                                <tr key={index}>
+                                    <td className="border px-4 py-2">
+                                        {doctor.name}
+                                    </td>
+                                    <td className="border px-4 py-2">
+                                        {doctor.wilaya}
+                                    </td>
+                                    <td className="border px-4 py-2">
+                                        {doctor.localisation}
+                                    </td>
+                                    <td className="border px-4 py-2">
+                                        <button className="bg-blue_v text-white px-4 py-2 rounded">
+                                            See Profile
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
             </div>
         </div>
     );
--- src/Router.jsx
+++ src/Router.jsx
@@ -6,20 +6,11 @@ import Default from "./Default";
 import Login from "./Components/Auth/Login/Login";
 
 import Users from "./Components/Users/Users";
-import Projects_Requests from "./Components/Projects/Requests/Requests.jsx";
-
-import Projects_Applications from "./Components/Projects/Applications/Applications.jsx";
-import Projects_Applications_Listof_Freelancers from "./Components/Projects/Applications/List.jsx";
-import Projects_Applications_Profile from "./Components/Projects/Applications/Profile.jsx";
-
-import Projects_Paying from "./Components/Projects/Paying/Paying.jsx";
-import Projects_Paying_item from "./Components/Projects/Paying/item.jsx";
 
 import Institustions from "./Components/Institustions/Institustions.jsx";
 import Institustions_item from "./Components/Institustions/Item.jsx";
 import New_institution from "./Components/Institustions/New_institution.jsx";
 
-import Request_Project_Item from "./Components/Projects/Requests/Item.jsx";
 import Feedbacks_Clients from "./Components/Feedbacks/Clients_Freedbacks/Clients_Freedbacks.jsx";
 import Feedbacks_Freelancers from "./Components/Feedbacks/Freelancers_Feedbacks/Freelancers_Feedbacks.jsx";
 import Home_Feedbacks from "./Components/Feedbacks/Home_Feedbacks/Home_Feedbacks.jsx";
@@ -76,42 +67,6 @@ const routes = createBrowserRouter([
             {
                 path: "/New_institution",
                 element: <New_institution />,
-                errorElement: <ErrorElement />,
-            },
-            {
-                path: "/Projects_Requests",
-                element: <Projects_Requests />,
-                errorElement: <ErrorElement />,
-            },
-            {
-                path: "/Projects_Requests/:id",
-                element: <Request_Project_Item />,
-                errorElement: <ErrorElement />,
-            },
-            {
-                path: "/Projects_Applications",
-                element: <Projects_Applications />,
-                errorElement: <ErrorElement />,
-            },
-            {
-                path: "/Projects_Applications/:projectId",
-                element: <Projects_Applications_Listof_Freelancers />,
-                errorElement: <ErrorElement />,
-            },
-            {
-                path: "/Projects_Applications/:projectId/:freelancerId",
-                element: <Projects_Applications_Profile />,
-                errorElement: <ErrorElement />,
-            },
-
-            {
-                path: "/Projects_Paying",
-                element: <Projects_Paying />,
-                errorElement: <ErrorElement />,
-            },
-            {
-                path: "/Projects_Paying/:projectId",
-                element: <Projects_Paying_item />,
                 errorElement: <ErrorElement />,
             },
 
